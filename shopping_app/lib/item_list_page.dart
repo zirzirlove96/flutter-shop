0f0cc2cfb6487a8e230df0c3e56a0e7af84d37c0
김지영 <zirzirlove96@github.com>
2025-05-04 23:32:44 +0900

    first commit

---
+++ shopping_app/lib/item_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemListPage extends StatefulWidget {
+  const ItemListPage({super.key});
+
+  @override
+  State<ItemListPage> createState() => _ItemListPageState();
+}
+
+class _ItemListPageState extends State<ItemListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
